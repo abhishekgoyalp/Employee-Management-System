e5f916a56047d772eae9c5dcb6e82e375504e89d
abhishek-goyal <abhishekgoyal5855@gmail.com>
2024-11-21 13:28:17 +0530

    Initial Commit

---
+++ src/main/java/com/employeemanagement/employee_management_system/EmployeeManagementSystem.java
@@ -0,0 +1,12 @@
+package com.employeemanagement.employee_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystem {
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystem.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeemanagement/employee_management_system/EmployeeManagementSystemTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
